55d3690280f68f2ce0b709e879b7d2362e5a0dea
Astridisbach <astridisbachsorensen@gmail.com>
2025-01-26 21:15:24 +0100

    opgave løst

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let birthYear = prompt("Enter your birth year:");
+
+birthYear = parseInt(birthYear);
+
+let currentYear = new Date().getFullYear();
+
+let age = currentYear - birthYear;
+
+console.log(`You are ${age} years old.`);
